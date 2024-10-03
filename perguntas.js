cfed8923d6325e7db349408a58b6d70dbea26e2e
FredericoFonsecaFerro <134947676+FredericoFonsecaFerro@users.noreply.github.com>
2024-10-03 16:28:53 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,21 +1,4 @@
 criaCartao(
-    categoria,
-    pergunta,
-    resposta
-)
-
-criaCartao(
-    categoria,
-    pergunta,
-    resposta
-)
-
-criaCartao(
-    categoria,
-    pergunta,
-    resposta
-)
-criaCartao(
     'Programação',
     'O que é Python?',
     'O Python é uma linguagem de programação'
@@ -31,4 +14,10 @@ criaCartao(
     'Programação',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
 )
